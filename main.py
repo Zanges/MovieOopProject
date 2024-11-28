e7b0bb055c867edec8133cc79a958f0b9c50ed9e
Zanges <3868054+Zanges@users.noreply.github.com>
2024-11-28 23:45:29 +0100

    added the command line argument for the storage file.

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import sys
+
 from movie_app import MovieApp
 from storage.storage_json import StorageJson
 from storage.storage_csv import StorageCSV
 
 
 def main():
-    while True:
+    storage = None
+    if sys.argv[1:]:
+        storage_choice = sys.argv[1]
+        if storage_choice.endswith(".json"):
+            storage = StorageJson(storage_choice)
+        elif storage_choice.endswith(".csv"):
+            storage = StorageCSV(storage_choice)
+        else:
+            print("Invalid file name argument. It will be IGNORED!")
+    while not storage:
         storage_choice = input("Which storage file do you want to use(json/csv)? Enter the file name or press enter for default[movies.json]: ")
         if storage_choice == "":
             storage = StorageJson("movies.json")
