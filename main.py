0d5c9a278d3efb54d2f30e9b0307f464c8096434
Zanges <3868054+Zanges@users.noreply.github.com>
2024-11-28 23:33:20 +0100

    added the ability to select the storage file.

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from movie_app import MovieApp
 from storage.storage_json import StorageJson
+from storage.storage_csv import StorageCSV
 
 
 def main():
-    storage = StorageJson("movies.json")
+    while True:
+        storage_choice = input("Which storage file do you want to use(json/csv)? Enter the file name or press enter for default[movies.json]: ")
+        if storage_choice == "":
+            storage = StorageJson("movies.json")
+            break
+        if storage_choice.endswith(".json"):
+            storage = StorageJson(storage_choice)
+            break
+        if storage_choice.endswith(".csv"):
+            storage = StorageCSV(storage_choice)
+            break
+        print("Invalid file name. Please try again.")
+
     app = MovieApp(storage)
     app.run()
 
